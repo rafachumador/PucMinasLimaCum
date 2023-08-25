a3eb87878c6176832e37524d16c56a42d776a951
rafachumador <99569855+rafachumador@users.noreply.github.com>
2023-08-25 03:44:20 -0300

    Add files via upload

---
+++ TI2/Exercício 1: Integração Eclipse e GitHub/Exercicio01/src/SomarDoisNumeros.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		int num1, num2, soma;
+		System.out.printf("Digite um número: ");
+		num1 = sc.nextInt();
+		System.out.printf("Digite outro número: ");
+		num2 = sc.nextInt();
+		soma = num1 + num2;
+		System.out.println("Soma: " + soma);
+	}
+}
